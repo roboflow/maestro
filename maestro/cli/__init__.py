c7c63b7ffb92557132c032598ea3b81cf6075d82
SkalskiP <piotr.skalski92@gmail.com>
2024-09-10 18:30:16 +0200

    fix

--- maestro/cli/__init__.py
+++ maestro/cli/__init__.py
@@ -1,5 +1,1 @@
-import typer
-from maestro.trainer.models.florence_2.entrypoint import app as florence2_app
 
-app = typer.Typer()
-app.add_typer(florence2_app, name="florence2")
--- maestro/cli/main.py
+++ maestro/cli/main.py
@@ -13,3 +13,12 @@
 
 # if __name__ == "__main__":
 #     app()
+
+import typer
+from maestro.trainer.models.florence_2.entrypoint import app as florence2_app
+
+app = typer.Typer()
+app.add_typer(florence2_app, name="florence2")
+
+if __name__ == "__main__":
+    app()
