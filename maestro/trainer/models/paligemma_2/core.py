5ce7ff1c8f11b9208e19900d4722586ccf349ef0
SkalskiP <piotr.skalski92@gmail.com>
2025-01-28 15:36:05 +0100

    save progress before switching to QWEN2.5VL

--- maestro/trainer/models/paligemma_2/core.py
+++ maestro/trainer/models/paligemma_2/core.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass, field, replace
+from functools import partial
 from typing import Literal, Optional
 
 import torch
 
+from maestro.trainer.common.data_loaders.datasets import create_data_loaders
 from maestro.trainer.common.utils.file_system import create_new_run_directory
 from maestro.trainer.common.utils.metrics import BaseMetric
 from maestro.trainer.common.utils.reproducibility import make_it_reproducible
@@ -11,6 +13,7 @@ from maestro.trainer.models.paligemma_2.checpoints import (
     DEFAULT_PALIGEMMA2_MODEL_REVISION,
     DEVICE, load_model, OptimizationStrategy,
 )
+from maestro.trainer.models.paligemma_2.loaders import train_collate_fn, evaluation_collate_fn
 
 
 @dataclass(frozen=True)
@@ -29,6 +32,13 @@ class Configuration:
         device (torch.device): Device to use for training.
         cache_dir (Optional[str]): Directory to cache the model.
         epochs (int): Number of training epochs.
+        lr (float): Learning rate for the optimizer.
+        batch_size (int): Batch size for training.
+        val_batch_size (Optional[int]): Batch size for validation.
+        num_workers (int): Number of workers for data loading.
+        val_num_workers (Optional[int]): Number of workers for validation data loading.
+        output_dir (str): Directory to save output files.
+        metrics (List[BaseMetric]): List of metrics to track during training.
     """
 
     dataset: str
@@ -38,6 +48,11 @@ class Configuration:
     optimization_strategy: Literal["lora", "qlora", "freeze", "none"] = "lora"
     cache_dir: Optional[str] = None
     epochs: int = 10
+    lr: float = 1e-5
+    batch_size: int = 4
+    val_batch_size: Optional[int] = None
+    num_workers: int = 0
+    val_num_workers: Optional[int] = None
 
     output_dir: str = "./training/paligemma2"
     metrics: list[BaseMetric] = field(default_factory=list)
@@ -74,4 +89,13 @@ def train(config: Configuration) -> None:
         device=config.device,
         optimization_strategy=OptimizationStrategy(config.optimization_strategy),
         cache_dir=config.cache_dir,
+    )
+    train_loader, val_loader, test_loader = create_data_loaders(
+        dataset_location=config.dataset,
+        train_batch_size=config.batch_size,
+        train_collect_fn=partial(train_collate_fn, processor=processor),
+        train_num_workers=config.num_workers,
+        test_batch_size=config.val_batch_size,
+        test_collect_fn=partial(evaluation_collate_fn, processor=processor),
+        test_num_workers=config.val_num_workers,
     )
