7572851946f8f4f694b279e21e12387b36a70d39
SkalskiP <piotr.skalski92@gmail.com>
2025-02-04 16:24:48 +0100

    make `train_collate_fn` return tokenized suffixes `suffixes` and not return attention_mask as it is redundant

--- maestro/trainer/models/florence_2/loaders.py
+++ maestro/trainer/models/florence_2/loaders.py
@@ -6,12 +6,20 @@ from PIL import Image
 def train_collate_fn(batch: list[tuple[Image.Image, dict[str, Any]]], processor):
     images, data = zip(*batch)
     prefixes = [entry["prefix"] for entry in data]
+    suffixes = [entry["suffix"] for entry in data]
     inputs = processor(text=prefixes, images=images, return_tensors="pt", padding=True)
 
     input_ids = inputs["input_ids"]
-    attention_mask = inputs["attention_mask"]
     pixel_values = inputs["pixel_values"]
-    return input_ids, attention_mask, pixel_values
+
+    labels = processor.tokenizer(
+        text=suffixes,
+        return_tensors="pt",
+        padding=True,
+        return_token_type_ids=False
+    ).input_ids
+
+    return input_ids, pixel_values, labels
 
 
 def evaluation_collate_fn(batch: list[tuple[Image.Image, dict[str, Any]]], processor):
@@ -21,6 +29,5 @@ def evaluation_collate_fn(batch: list[tuple[Image.Image, dict[str, Any]]], proce
     inputs = processor(text=prefixes, images=images, return_tensors="pt", padding=True)
 
     input_ids = inputs["input_ids"]
-    attention_mask = inputs["attention_mask"]
     pixel_values = inputs["pixel_values"]
-    return input_ids, attention_mask, pixel_values, prefixes, suffixes
+    return input_ids, pixel_values, prefixes, suffixes
